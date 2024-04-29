3b6a05976b9a350222ba6827ffaa3b9699770113
yassa <yassatamer99@gmail.com>
2024-04-30 02:15:19 +0300

    refactor the bottom bar

---
+++ lib/pages/apply_for_volunteering_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ApplyForVolunteeringPage extends StatefulWidget {
+  const ApplyForVolunteeringPage({super.key});
+
+  @override
+  State<ApplyForVolunteeringPage> createState() => _ApplyForVolunteeringPageState();
+}
+
+class _ApplyForVolunteeringPageState extends State<ApplyForVolunteeringPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/make_donation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MakeDonationPage extends StatefulWidget {
+  const MakeDonationPage({super.key});
+
+  @override
+  State<MakeDonationPage> createState() => _MakeDonationPageState();
+}
+
+class _MakeDonationPageState extends State<MakeDonationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
