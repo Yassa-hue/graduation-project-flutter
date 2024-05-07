711400a8ae75d129897e8ba3153c0f20fe237604
ihab4real <ihabadham4@gmail.com>
2024-05-07 23:25:48 +0300

    make login page keyboard dismissible on tap.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:graduationproject/utils/AuthProvider.dart';
 import 'package:graduationproject/pages/home_page.dart';
 import 'package:graduationproject/pages/reset_password_page.dart';
+import 'package:graduationproject/utils/utils.dart';
 import '../../../utils/images_paths.dart';
 import '../components/custom_button.dart';
 import '../components/custom_field.dart';
@@ -48,101 +49,102 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            padding: const EdgeInsets.fromLTRB(25, 10, 25, 0),
-            child: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                        child: Container(
-                      padding: const EdgeInsets.only(top: 40),
-                      height: 250,
-                      child: Image.asset(
-                        ImagesPaths.undraw,
-                        width: 210,
-                        fit: BoxFit.cover,
-                      ),
-                    )),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Text(
-                      'Welcome Back',
+        body: DismissKeyboardOnTap(
+      child: Container(
+          padding: const EdgeInsets.fromLTRB(25, 10, 25, 0),
+          child: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Center(
+                  child: Container(
+                padding: const EdgeInsets.only(top: 40),
+                height: 250,
+                child: Image.asset(
+                  ImagesPaths.undraw,
+                  width: 210,
+                  fit: BoxFit.cover,
+                ),
+              )),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'Welcome Back',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: PRIMARY_COLOR),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomField(
+                text: "Email",
+                hiinttext: "ys198@mail.com",
+                prefex: Icons.email,
+                onChanged: (value) => {
+                  setState(() {
+                    email = value;
+                  })
+                },
+              ),
+              CustomField(
+                text: "Password",
+                hiinttext: "********",
+                prefex: Icons.lock_person,
+                suffex: Icons.visibility_off,
+                onChanged: (value) => {
+                  setState(() {
+                    password = value;
+                  })
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Checkbox(
+                    value: rememberMe,
+                    onChanged: (val) {
+                      setState(() {
+                        rememberMe = val!;
+                      });
+                    },
+                    side: const BorderSide(
+                        color: PRIMARY_COLOR,
+                        style: BorderStyle.solid,
+                        width: 2),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                  ),
+                  const Text(
+                    'Remember me',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 17),
+                  ),
+                  const SizedBox(),
+                  TextButton(
+                    onPressed: () => navigateToResetPasswordPage(),
+                    child: const Text(
+                      'Forget Password?',
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          color: PRIMARY_COLOR),
+                          color: PRIMARY_COLOR,
+                          fontSize: 17),
                     ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    CustomField(
-                      text: "Email",
-                      hiinttext: "ys198@mail.com",
-                      prefex: Icons.email,
-                      onChanged: (value) => {
-                        setState(() {
-                          email = value;
-                        })
-                      },
-                    ),
-                    CustomField(
-                      text: "Password",
-                      hiinttext: "********",
-                      prefex: Icons.lock_person,
-                      suffex: Icons.visibility_off,
-                      onChanged: (value) => {
-                        setState(() {
-                          password = value;
-                        })
-                      },
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Checkbox(
-                          value: rememberMe,
-                          onChanged: (val) {
-                            setState(() {
-                              rememberMe = val!;
-                            });
-                          },
-                          side: const BorderSide(
-                              color: PRIMARY_COLOR,
-                              style: BorderStyle.solid,
-                              width: 2),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5)),
-                        ),
-                        const Text(
-                          'Remember me',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                              fontSize: 17),
-                        ),
-                        const SizedBox(),
-                        TextButton(
-                          onPressed: () => navigateToResetPasswordPage(),
-                          child: const Text(
-                            'Forget Password?',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: PRIMARY_COLOR,
-                                fontSize: 17),
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    CustomButton(
-                      onTap: () => login(),
-                      title: "Login",
-                    )
-                  ]),
-            )));
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomButton(
+                onTap: () => login(),
+                title: "Login",
+              )
+            ]),
+          )),
+    ));
   }
 }
