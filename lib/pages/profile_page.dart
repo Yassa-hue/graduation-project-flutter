7b7b3e28f45a1b76db6b5182280d0b61d242e7f2
yassa <yassatamer99@gmail.com>
2024-06-04 18:56:02 +0300

    implement the logic

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:graduationproject/components/profile_page/activity_list.dart';
 import 'package:graduationproject/components/profile_page/user_details.dart';
-import 'package:graduationproject/models/campaign_model.dart';
-import 'package:graduationproject/models/donation_model.dart';
-import 'package:graduationproject/models/volunteering_activity_model.dart';
+import 'package:graduationproject/services/campaign_service.dart';
+import 'package:graduationproject/services/donation_service.dart';
+import 'package:graduationproject/services/volunteering_activity_service.dart';
 import 'package:graduationproject/utils/AuthProvider.dart';
 import 'package:graduationproject/components/custom_app_bar.dart';
 import 'package:graduationproject/models/user_model.dart';
@@ -22,64 +22,28 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   void initState() {
     super.initState();
-    setState(() {
+    Future.delayed(Duration.zero, () async {
       currentUser = AuthProvider.of(context)!.currentUser;
-      activities = _getActivitiesForUser(currentUser);
+      activities = await _getActivitiesForUser(currentUser);
+      setState(() {});
     });
   }
 
-  List<dynamic> _getActivitiesForUser(UserModel? user) {
-    // Sample data for demonstration purposes
+  Future<List<dynamic>> _getActivitiesForUser(UserModel? user) async {
     if (user == null) return [];
 
     switch (user.role) {
       case UserRole.donor:
-        return [
-          Donation(
-              donorId: user.id,
-              amount: 100.0,
-              receivingOrganizationId: "org1",
-              periodicity: Periodicity.monthly,
-              createdAt: DateTime.now()),
-          Donation(
-              donorId: user.id,
-              amount: 200.0,
-              receivingOrganizationId: "org2",
-              periodicity: Periodicity.weekly,
-              createdAt: DateTime.now()),
-        ];
+        try {
+          return await DonationService().getDonationsByDonor(user.id);
+        } catch (error) {
+          return [];
+        }
       case UserRole.volunteer:
-        return [
-          VolunteeringActivityModel(
-              id: "1",
-              campaignId: "camp1",
-              organizationId: "org1",
-              numberOfHours: 5,
-              role: "Helper"),
-          VolunteeringActivityModel(
-              id: "2",
-              campaignId: "camp2",
-              organizationId: "org2",
-              numberOfHours: 10,
-              role: "Coordinator"),
-        ];
+        return await VolunteeringActivityModelService()
+            .getVolunteeringActivitiesByVolunteer(user.id);
       case UserRole.organization:
-        return [
-          Campaign(
-              id: "1",
-              coverImageLink: "https://example.com/cover1.jpg",
-              title: "Campaign 1",
-              description: "Description 1",
-              createdAt: DateTime.now(),
-              organizationId: user.id),
-          Campaign(
-              id: "2",
-              coverImageLink: "https://example.com/cover2.jpg",
-              title: "Campaign 2",
-              description: "Description 2",
-              createdAt: DateTime.now(),
-              organizationId: user.id),
-        ];
+        return await CampaignService().getCampaignsByOrganization(user.id);
       default:
         return [];
     }
@@ -109,42 +73,46 @@ class _ProfilePageState extends State<ProfilePage> {
           ]),
       body: Container(
         padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                  child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(150)),
-                    border: Border.all(color: Colors.grey)),
-                height: 150,
-                width: 150,
-                child: currentUser != null
-                    ? ClipRRect(
-                        borderRadius: BorderRadius.circular(150),
-                        child: Image.network(
-                          currentUser!.profileImageUrl,
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    : const CircularProgressIndicator(),
-              )),
-              const SizedBox(height: 25),
-              UserDetails(name: currentUser!.name, email: currentUser!.email),
-              const SizedBox(height: 40),
-              Text(
-                _getActivitiesTitle(currentUser!.role),
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              ActivityList(role: currentUser!.role, activities: activities),
-            ],
+        child: currentUser == null ? 
+          const CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ) :
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                    child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(150)),
+                      border: Border.all(color: Colors.grey)),
+                  height: 150,
+                  width: 150,
+                  child: currentUser != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(150),
+                          child: Image.network(
+                            currentUser!.profileImageUrl,
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : const CircularProgressIndicator(),
+                )),
+                const SizedBox(height: 25),
+                UserDetails(name: currentUser!.name, email: currentUser!.email),
+                const SizedBox(height: 40),
+                Text(
+                  _getActivitiesTitle(currentUser!.role),
+                  style:
+                      const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                ActivityList(role: currentUser!.role, activities: activities),
+              ],
+            ),
           ),
-        ),
       ),
     );
   }
