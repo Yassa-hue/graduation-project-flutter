9a7325eed53748ca95769a6d68d1966269cef83d
Yassa Tamer <58339464+Yassa-hue@users.noreply.github.com>
2024-06-07 09:50:35 +0300

    Fix minor issues on the home page (#55)
    * fix the action button
    
    * add campaign details

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:graduationproject/components/custom_app_bar.dart';
 import 'package:graduationproject/pages/page_manager.dart';
+import 'package:graduationproject/models/user_model.dart';
+import 'package:graduationproject/utils/AuthProvider.dart';
 
 import 'package:graduationproject/utils/color_palette.dart';
 import 'package:graduationproject/utils/constants.dart';
@@ -18,6 +20,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void onActionButtonPressed() {
+    int togoPageId = 0;
+
+    switch (AuthProvider.of(context)?.currentUser!.role) {
+      case UserRole.donor:
+        togoPageId = donationPage;
+        break;
+      case UserRole.organization:
+        togoPageId = campainPage;
+        break;
+      case UserRole.volunteer:
+        // TODO: Fix this when creating the volunteer pages
+        togoPageId = campainPage;
+        break;
+      default:
+        togoPageId = homePage;
+    }
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MainPage(
+                  currentPage: togoPageId,
+                )));
+  }
+
+  String getActionButtonMsg() {
+    switch (AuthProvider.of(context)?.currentUser!.role) {
+      case UserRole.donor:
+        return "Make Donation >";
+      case UserRole.organization:
+        return "Create Campaign >";
+      case UserRole.volunteer:
+        return "Volunteer >";
+      default:
+        return "Make Donation";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -128,16 +169,50 @@ class _HomePageState extends State<HomePage> {
                         itemBuilder: (context, index) {
                           return Container(
                             width: 300,
-                            height: 120,
+                            height: 130,
                             padding: const EdgeInsets.fromLTRB(0, 8, 10, 10),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                campaigns[index].coverImageLink,
-                                width: 300,
-                                height: 120,
-                                fit: BoxFit.cover,
-                              ),
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    campaigns[index].coverImageLink,
+                                    width: 300,
+                                    height: 130,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  left: 0,
+                                  right: 0,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          campaigns[index].title,
+                                          style: TextStyle(
+                                            color: Colors.grey[800],
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                        SizedBox(height: 4),
+                                        Text(
+                                          campaigns[index].description,
+                                          style: TextStyle(
+                                            color: Colors.grey[600],
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           );
                         },
@@ -158,17 +233,10 @@ class _HomePageState extends State<HomePage> {
                 height: 100,
                 width: double.infinity,
                 child: MaterialButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MainPage(
-                                  currentPage: donationPage,
-                                )));
-                  },
+                  onPressed: onActionButtonPressed,
                   color: Colors.black,
-                  child: const Text(
-                    "Donate Now  >",
+                  child: Text(
+                    getActionButtonMsg(),
                     style: TextStyle(color: Colors.white, fontSize: 22),
                   ),
                 ),
