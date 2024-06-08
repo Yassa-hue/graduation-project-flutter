bef88e11cd1b5e1a69571ecafd010d83c58a64c9
yassa <yassatamer99@gmail.com>
2024-06-08 22:12:04 +0300

    fix the notifications

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -3,13 +3,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:graduationproject/models/user_model.dart';
 
 class NotificationService {
-  final CollectionReference _colectionReference = FirebaseFirestore.instance.collection('notifications');
+  final CollectionReference _colectionReference =
+      FirebaseFirestore.instance.collection('notifications');
 
-  Future<NotificationModel> createNotification(NotificationModel notification) async {
-    DocumentReference docRef = await _colectionReference.add(notification.toJson());
+  Future<NotificationModel> createNotification(
+      NotificationModel notification) async {
+    DocumentReference docRef =
+        await _colectionReference.add(notification.toJson());
     String id = docRef.id;
     await docRef.update({'id': id});
-    
+
     notification.id = id;
     return notification;
   }
@@ -17,14 +20,17 @@ class NotificationService {
   Future<NotificationModel> getNotificationById(String id) async {
     DocumentSnapshot snapshot = await _colectionReference.doc(id).get();
     if (snapshot.exists) {
-      return NotificationModel.fromJson(snapshot.data() as Map<String, dynamic>);
+      return NotificationModel.fromJson(
+          snapshot.data() as Map<String, dynamic>);
     } else {
       throw Exception('Notification not found');
     }
   }
 
   Future<void> updateNotification(NotificationModel notification) async {
-    await _colectionReference.doc(notification.id).update(notification.toJson());
+    await _colectionReference
+        .doc(notification.id)
+        .update(notification.toJson());
   }
 
   Future<void> deleteNotification(String id) async {
@@ -32,19 +38,24 @@ class NotificationService {
   }
 
   Future<List<NotificationModel>>? getNotificationsByUser(String userId) async {
-    QuerySnapshot snapshot = await _colectionReference.where('userId', isEqualTo: userId).get();
-    
-    return snapshot
-        .docs
-        .map((doc) => NotificationModel.fromJson(doc.data() as Map<String, dynamic>))
+    QuerySnapshot snapshot =
+        await _colectionReference.where('userId', isEqualTo: userId).get();
+
+    return snapshot.docs
+        .map((doc) =>
+            NotificationModel.fromJson(doc.data() as Map<String, dynamic>))
         .toList();
   }
 
-  Future<void> sendNotificationToUsersByType(NotificationModel notification, UserRole userRole) async {
-    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('users').where('role', isEqualTo: userRole).get();
-    
+  Future<void> sendNotificationToUsersByType(
+      NotificationModel notification, UserRole userRole) async {
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('role', isEqualTo: userRole.toString().split('.').last)
+        .get();
+
     List<String> userIds = snapshot.docs.map((doc) => doc.id).toList();
-    
+
     for (String userId in userIds) {
       notification.userId = userId;
       await createNotification(notification);
